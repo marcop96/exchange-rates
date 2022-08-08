79c8734733097ec5800a207ec22fad9c37c774fe
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-08-08 18:35:44 +0200

    declare html variables in js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-// #DA9F93
-//#EBD4CB
-//#B6465F
-//colors
+const $amountInput = document.querySelector("#amount-input");
+const $initialSelector = document.querySelector("#initial-currency-selector");
+const $finalSelector = document.querySelector("#final-currency-selector");
+const $convertButton = document.querySelector("#convert-button");
+const $resultText = document.querySelector(".result-text");
