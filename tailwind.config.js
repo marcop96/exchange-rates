81903494b20ef439b5f64fd441b228edde1d3b06
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-08-08 14:32:16 +0200

    deletes css classes and adds them to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      navcol: "#b6465f",
+      pinky: "#ebd4cb",
+    },
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: false,
+  },
 };
