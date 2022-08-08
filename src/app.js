59edaec4a907e456929ee63bc29c8c51359f592d
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-08-08 21:52:32 +0200

    calls api

--- src/app.js
+++ src/app.js
@@ -3,3 +3,16 @@ const $initialSelector = document.querySelector("#initial-currency-selector");
 const $finalSelector = document.querySelector("#final-currency-selector");
 const $convertButton = document.querySelector("#convert-button");
 const $resultText = document.querySelector(".result-text");
+let conversion = "";
+// fetch https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD
+
+$convertButton.addEventListener('click')function () {
+  fetch(
+    "https://v6.exchangerate-api.com/v6/517e16350fa29778db9d180f/pair/EUR/GBP"
+  )
+    .then((response) => response.json())
+    .then(
+      (data) =>
+        ($resultText.textContent = `${$amountInput} Eur are ${data.conversion_rate} GBP`)
+    );
+});
