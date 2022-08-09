c243d919617b6169d74befb849d25a8238ce6059
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-08-09 17:38:27 +0200

    refactor put data in functions

--- src/app.js
+++ src/app.js
@@ -3,30 +3,36 @@ const $initialSelector = document.querySelector("#initial-currency-selector");
 const $finalSelector = document.querySelector("#final-currency-selector");
 const $convertButton = document.querySelector("#convert-button");
 const $resultText = document.querySelector(".result-text");
-let conversion = "";
-// fetch https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD
+let baseCurrency = "";
+let conversionRate;
+let targetCurrency = "";
 
-// $convertButton.onlick = function () {
-// fetch(
-//   "https://v6.exchangerate-api.com/v6/517e16350fa29778db9d180f/pair/EUR/GBP"
-// )
-//   .then((response) => response.json())
-//   .then(
-//     (data) =>
-//       ($resultText.textContent = `${$amountInput.value} Eur are ${data.conversion_rate} GBP`)
-//   );
-// };
+fetch("https://v6.exchangerate-api.com/v6/517e16350fa29778db9d180f/latest/USD")
+  .then((response) => response.json())
+  .then((data) => console.log(data.conversion_rates));
 
-//($resultText.textContent = `${$amountInput.value} Eur are ${data.conversion_rate} GBP`)
 $convertButton.onclick = function () {
+  getData();
+  showConversion();
+};
+
+function getData() {
   fetch(
     "https://v6.exchangerate-api.com/v6/517e16350fa29778db9d180f/pair/USD/ARS"
   )
     .then((response) => response.json())
-    .then(
-      (data) =>
-        ($resultText.textContent = `${$amountInput.value} ${
-          data.base_code
-        } are ${data.conversion_rate * $amountInput.value} ${data.target_code}`)
-    );
-};
+    .then((data) => {
+      console.log(data);
+      baseCurrency = data.base_code;
+      console.log(baseCurrency);
+      baseCurrency = data.base_code;
+      targetCurrency = data.target_code;
+      conversionRate = data.conversion_rate;
+    });
+}
+
+function showConversion() {
+  $resultText.textContent = `${$amountInput.value} ${baseCurrency} are ${
+    conversionRate * $amountInput.value
+  } ${targetCurrency}`;
+}
