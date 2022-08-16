a47b6da92c38ed449973c021a5cd6a4c1061a64d
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-08-16 12:51:30 +0200

    removes outline in html
    so when an error happens then it gets fixed there-s no red outline

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const $initialSelector = document.querySelector("#initial-currency-selector");
 const $finalSelector = document.querySelector("#final-currency-selector");
 const $convertButton = document.querySelector("#convert-button");
 const $resultText = document.querySelector(".result-text");
+const $resultContainer = document.querySelector(".result-container");
 let baseCurrency = "";
 let conversionRate;
 let targetCurrency = "";
@@ -31,6 +32,7 @@ $convertButton.onclick = function () {
   getURL();
   getData();
   showConversion();
+  $resultContainer.classList.remove("hidden");
 };
 
 setTimeout(addOptions, 500);
@@ -81,10 +83,12 @@ function validateAmount(amount) {
   if (amount.length === 0) {
     console.log("field must contain at least one number");
     $amountInput.classList.add("outline");
+    $amountInput.classList.add("outline-red-500");
     return "field must contain at least one number";
   }
   if (amount.length > 0) {
     $amountInput.classList.remove("outline");
+    $amountInput.classList.remove("outline-red-500");
   }
   // if ("d+(.d+)?(?=$| )".test(amount)) {
   //   console.log("only numbers");
@@ -94,10 +98,12 @@ function validateInitialSelect(select) {
   if (select == "Choose a currency") {
     console.log("please select an initial currency");
     $initialSelector.classList.add("outline");
+    $initialSelector.classList.add("outline-red-500");
     return "Please select an initial currency";
   }
   if (select != "Choose a currency") {
     $initialSelector.classList.remove("outline");
+    $initialSelector.classList.remove("outline-red-500");
   } else {
     return;
   }
@@ -106,10 +112,12 @@ function validateFinalSelect(select) {
   if (select == "Choose a currency") {
     console.log("please select a final currency");
     $finalSelector.classList.add("outline");
+    $finalSelector.classList.add("outline-red-500");
     return "Please select a final currency";
   }
   if (select != "Choose a currency") {
     $finalSelector.classList.remove("outline");
+    $finalSelector.classList.remove("outline-red-500");
   } else {
     return;
   }
